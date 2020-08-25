f60ca0ca3d162b446ab719f4a115701ba7740e06
Lucas Maia <lucas.maia435@outlook.com>
2020-08-25 18:36:39 -0300

    Challenge done �

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from 'services/api';
 
 import "./styles.css";
 
 function App() {
   async function handleAddRepository() {
     // TODO
+    const response = await api.post('repositories',{
+      title: 'Umbriel',
+      url: 'https://githubl.com/rocketseat/umbriel',
+      techs: ['Node.js','ReactJS']
+    });
+
+    setRepos([ ...Repos, response.data]);
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepos(Repos.filter(repo => repo.id !== id));
   }
+
+  const [Repos, setRepos] = useState([]);
+
+  useEffect(()=>{
+    api.get('repositories').then(response => {
+      setRepos(response.data);
+    });
+  },[]);
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {Repos.map(repo => (
+          <li key={repo.id}>{repo.title}<button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
-          </button>
-        </li>
+            </button></li>
+        )
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
